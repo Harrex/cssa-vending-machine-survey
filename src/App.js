b5d1abd01ca54270ebf9dd00b6538fa010011b92
Harrex <h@rrex.au>
2024-01-07 22:37:38 +1100

    built the core. It's pretty hacky at the moment, and there's no CSS, but it does work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+const KEY = "vendingMachineDrinksSavedSurveyInfo" // If anything else uses this I'd like to see it
+
+function DrinksForm () {
+    function getData () {
+        const dataInStorage = localStorage.getItem(KEY);
+        if ( dataInStorage ) {
+            return (dataInStorage); // Check if this is empty ie. First time user
+        } else {
+            return JSON.stringify({
+                "coke": 0,
+                "pepsi": 0
+            });
+        }
+    }
+    const data = getData();
+    const [dataJSON, setDataJSON] = useState(JSON.parse(data));
+    // I hate this but I can't think of another way to get
+    // the right scope without muts. yayy FP
+
+    function saveFormToLocalStorage(dataToUse = 0) {
+        console.log(dataToUse);
+        localStorage.setItem(KEY, JSON.stringify(dataToUse));
+    }
+
+    function pressAButton ({key}) {
+        // Do I actually have to pass strings around and then JSON them for a
+        // second and then string them again? I hate that.
+        console.log(key);
+        const newDataJSON = {
+            ...dataJSON,
+            [key]: (dataJSON[key]) + 1
+        }
+        console.log(newDataJSON);
+        setDataJSON(newDataJSON);
+        saveFormToLocalStorage(newDataJSON);
+    }
+      const [drinkInputName, setDrinkInputName] = useState(''); // State for the input field                                                                                                                
+                                                                                                                                                                                        
+      const handleDrinkFormSubmit = (event) => {                                                                                                                                                 
+        event.preventDefault();                                                                                                                                                         
+        setDataJSON({ ...dataJSON, [drinkInputName]: 1 }); // Add the 'name' key to the data object                                                                                                      
+      };                                                                                                                                                                                
+                                                                                                                                                                                        
+      const handleDrinkFormChange = (event) => {                                                                                                                                                 
+        setDrinkInputName(event.target.value); // Update the 'name' state when the input changes                                                                                                  
+      };                                                                                                                                                                                
+                                                                                                                                                                                        
+    return (
+        <div>
+            {Object.keys(dataJSON).map((key, index) => 
+                <button key={key} onClick={() => pressAButton({key})}>
+                    {key}
+                </button>
+            )}
+            <form onSubmit={handleDrinkFormSubmit}>
+                <input type="text"
+                value={drinkInputName}
+                onChange={handleDrinkFormChange}
+                placeholder="New Drink?"/>
+                <input type="submit" value="Submit" />
+            </form>
+            <p>{JSON.stringify(dataJSON)}</p>
+        </div>
+    )
+}
+// Unnecessary Save Button
+// <button onClick={() => saveFormToLocalStorage(dataJSON)}>Save Form</button>
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DrinksForm />
       </header>
     </div>
   );
